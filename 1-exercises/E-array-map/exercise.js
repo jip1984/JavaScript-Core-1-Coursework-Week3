a4c20c6e1fb99cae6082c1611feb3bb614d5b9c4
Jon Prosser <58789786+jip1984@users.noreply.github.com>
2021-01-05 21:11:27 +0000

    Update exercise.js

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -1,9 +1,20 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-function times(number){
+
+function timesHundred (num) {
     return number * 100;
 }
+
+// function expression
+let timesHundred = numbers.map(function (num){
+    return num * 100;
+})
+
+// arrow function
+let timesHundred = numbers.map(num => {
+        return num * 100
+});
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
